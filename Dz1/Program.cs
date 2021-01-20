a50aa0e3e2f751ef25da7024766de2e522923830
AlexeyLin <aleksey.lin@gmail.com>
2021-01-20 15:06:32 +0300

    Dz1 done

---
+++ Dz1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Dz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свое имя: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Привет, {userName}, сегодня {DateTime.Now.ToString("d")}!");
+        }
+    }
+}
