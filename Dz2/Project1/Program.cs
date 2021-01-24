f02c5aaaa5c9c79d9b2fb09e24b6a6ca022ff69f
AlexeyLin <aleksey.lin@gmail.com>
2021-01-24 22:43:37 +0300

    Dz2 done

---
+++ Dz2/Project1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Project1
+{
+    class Program
+    {
+        enum MonthOfYear
+        {
+            январь = 1,
+            февраль,
+            март,
+            апрель,
+            май,
+            июнь,
+            июль,
+            август,
+            сентябрь,
+            октябрь,
+            ноябрь,
+            декабрь
+        }
+        static void Main(string[] args)
+        {
+            //Задание с температурой выполнил через float, потому что хотел чтобы полградуса не терялись.
+            Console.Write("Введите минимальную температуру сегодня(°C): ");
+            int minTemp = int.Parse(Console.ReadLine());
+            Console.Write("Введите максимальную температуру сегодня(°C): ");
+            int maxTemp = int.Parse(Console.ReadLine());
+            float midTemp = (float)(minTemp + maxTemp) / 2; 
+            Console.WriteLine($"Среднесуточная температура сегодня: {midTemp}°C");
+
+            //Задание текущего месяца
+            Console.Write("Введите номер текущего месяца: ");
+            int monthNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Текущий месяц: {(MonthOfYear)monthNum}");
+
+            //Задание определения дождливой зимы
+            if ((monthNum == 12 || monthNum < 3) && midTemp > 0)
+            {
+                Console.WriteLine("Дождливая зима");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Dz2/Project2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number%2 == 0)
+            {
+                Console.WriteLine("Введенное число является четным.");
+                return;
+            }
+            Console.WriteLine("Введенное число является нечетным.");
+            Console.ReadKey();
+        }
+    }
+}
