71128a18f8324d2f9dbf564bec5f38919456f221
AlexeyLin <aleksey.lin@gmail.com>
2021-01-26 17:35:20 +0300

    Dz3 done

---
+++ Dz3/Project 4/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Project4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] seaBattle = new string[10, 10];
+
+            //однопалубные
+            seaBattle[6, 7] = "X";
+            seaBattle[8, 3] = "X";
+            seaBattle[8, 5] = "X";
+            seaBattle[9, 0] = "X";
+
+            //двухпалубные
+            for(int i = 0; i<2; i++)
+            {
+                seaBattle[0, i] = "X";
+            }
+            for (int i = 2; i < 4; i++)
+            {
+                seaBattle[i, 9] = "X";
+            }
+            for (int i = 8; i < 10; i++)
+            {
+                seaBattle[i, 8] = "X";
+            }
+
+            //трехпалубные
+            for (int i = 0; i < 3; i++)
+            {
+                seaBattle[i, 3] = "X";
+            }
+
+            for (int i = 4; i < 7; i++)
+            {
+                seaBattle[i, 1] = "X";
+            }
+
+            //четырехпалубные
+            for (int i = 6; i < 10; i++)
+            {
+                seaBattle[0, i] = "X";
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if(seaBattle[i,j]!="X")
+                    {
+                        seaBattle[i, j] = "O";
+                    }
+                    Console.Write(seaBattle[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ Dz3/Project2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] phoneBook =
+            {
+                {"Иван Абрамов","8-896-123-24-01"},
+                {"Катя Иванова","8-896-123-24-02"},
+                {"Игорь Абрамов","8-896-123-24-03"},
+                {"Алексей Абрамов","8-896-123-24-04"},
+                {"Леонид Абрамов","8-896-123-24-05"}
+            };
+            string searchName;
+            bool noName;
+
+            Console.WriteLine("Здравствуйте. Вас приветствует телефонный справочник.");
+            while (true)
+            {
+                Console.WriteLine("Пожалуйста введите имя и фамилию интересующего вас абонента: ");
+                searchName = Console.ReadLine();
+                noName = true;
+                for (int i = 0; i < phoneBook.GetLength(0); i++)
+                {
+                    if (phoneBook[i, 0] == searchName)
+                    {
+                        Console.WriteLine($"{phoneBook[i, 0]} {phoneBook[i, 1]}");
+                        noName = false;
+                        break;
+                    }
+                }
+                if (noName == true)
+                {
+                    Console.WriteLine("Интересующий вас абонент отсутствует в телефонной книге.");
+                }
+            }
+        }
+    }
+}
